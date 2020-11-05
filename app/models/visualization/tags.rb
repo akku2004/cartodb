f5c862ef43d8270bef95db355d960a457f5ef9c6
Alberto Miedes Garcés <albertomg994@gmail.com>
2020-11-05 12:16:12 +0100

    Migrate SharedEntity to ActiveRecord

--- app/models/visualization/tags.rb
+++ app/models/visualization/tags.rb
@@ -98,12 +98,10 @@ module CartoDB
 
         only_shared = only_shared?(params)
 
-        ids = CartoDB::SharedEntity.where({
+        ids = Carto::SharedEntity.where(
           recipient_id: @user.id,
-          entity_type: CartoDB::SharedEntity::ENTITY_TYPE_VISUALIZATION
-        }).all.map { |entity|
-          entity.entity_id
-        }
+          entity_type: Carto::SharedEntity::ENTITY_TYPE_VISUALIZATION
+        ).pluck(:entity_id)
         return '' if ids.nil? || ids.empty?
 
         if only_shared
